45b8735aed9e151a063dcf7ceda770e03b2dc6db
pocoyo <joshuaelson46@gmail.com>
2025-10-28 01:58:47 +0800

    (chore) reconfigure typescript and jest setting

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 // to use es6module inside ts jest file
 export default {
-  preset: "ts-jest/presets/default-esm",         // use ts-jest for TypeScript files
-  testEnvironment: "node",   // Node environment
+  preset: "ts-jest/presets/default-esm", // use ts-jest for TypeScript files
+  testEnvironment: "node", // Node environment
   moduleFileExtensions: ["ts", "js", "json", "node"], // file types Jest should process
   testMatch: ["**/*.test.ts"], // match test files
-   transform: {
-    '^.+\\.tsx?$': ['ts-jest'],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest"],
   },
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
 };
