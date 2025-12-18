1c36a6b609af3e8c67b8b2af4fba6e8aa8269848
pocoyo <joshuaelson46@gmail.com>
2025-12-18 08:53:59 +0800

    feat(profile): add profile endpoint - Add Get /Profile/getProfile endpoint - Route protected with JWT authentication - Return user profile information :

--- __tests__/Router/ProfileRouter.test.ts
+++ __tests__/Router/ProfileRouter.test.ts
@@ -1,18 +1,85 @@
 import { App } from "supertest/types";
 import { Server } from "../../src/Server/Server";
+import User, { UserRole } from "../../src/Controller/User/User";
+import Profile from "../../src/Controller/Profile/Profile";
+import { ProfileStatus } from "../../src/Controller/Profile/Profile.interface";
+import Env from "../../src/Config/config";
 import request from "supertest";
-import { response } from "express";
 
 describe("Profile Router Test",()=>{
-   let serverApp: App
 
+   let initializeDummyUser = ()=>{
+      let dummyEmail = "dummyEmail";
+      let dummyId = "dummyId";
+      let dummyPassword = "dummyPassword";
+      let dummyRole = UserRole.GUEST;
+      let dummyCreatedAt = new Date;
+
+      return User.tests__createTestUser(dummyId,dummyEmail,dummyPassword,dummyRole,dummyCreatedAt);
+   }
+   
+   let initializeDummyProfile = ()=>{
+      let dummyId = "dummyId";
+      let dummyUserName = "dummyUserName";
+      let dummyFirstName = "dummyFirstName";
+      let dummyLastName = "dummyLastName";
+      let dummyContact = "dummyContact";
+      let dummyAddress = "dummyAddress";
+      let dummyMembershipDate = new Date;
+      let dummyStatus = ProfileStatus.ACTIVE;
+      let dummyTotalFines = 0.0;
+      let dummyUpdatedAt = new Date;
+
+      return Profile.Tests__CreateProfile__(
+         {
+            user_id : dummyId,
+            user_name: dummyUserName,
+            first_name:dummyFirstName,
+            last_name: dummyLastName,
+            contact: dummyContact,
+            address: dummyAddress,
+            membership_date: dummyMembershipDate,
+            status: dummyStatus,
+            total_fines: dummyTotalFines,
+            updated_at: dummyUpdatedAt,
+         }
+      )
+   }
+
+   let serverApp: App;
+   let dummyUser:User = initializeDummyUser();
+   let dummyToken = Env.getGenerateJwtToken(dummyUser);
+   let dummyProfile:Profile = initializeDummyProfile();
+
+   let profileMock:any;
+   
    beforeAll(()=>{
       const serverInstance:Server = Server.getInstance(); 
       serverApp = (serverInstance as any).app;
    })
-
-   it("Should work", async ()=>{
-      const response = await request(serverApp).get("/profile/");
-      console.log(response.body);
+   beforeEach(()=>{
+      profileMock = jest.spyOn(Profile,"GetByUserId")
+      profileMock.mockResolvedValue(dummyProfile);
    })
+
+   it("get /profile/getProfile route should return error when request is unauthorized",async ()=>{
+      const response = await request(serverApp).get("/profile/getProfile");
+      expect(response.body).toHaveProperty("error");
+      expect(response.body.error).toBe("Unauthorized Request");
+   })
+
+   it("get /profile/getProfile route should return profile information",async ()=>{
+      const response = await request(serverApp).get("/profile/getProfile").set("Authorization",dummyToken)
+      expect(response.body).toHaveProperty("user_id");
+      expect(response.body).toHaveProperty("user_name");
+      expect(response.body).toHaveProperty("first_name");
+      expect(response.body).toHaveProperty("last_name");
+      expect(response.body).toHaveProperty("contact");
+      expect(response.body).toHaveProperty("address");
+      expect(response.body).toHaveProperty("membership_date");
+      expect(response.body).toHaveProperty("total_fines");
+      expect(response.body).toHaveProperty("status");
+      expect(response.body).toHaveProperty("updated_at");
+   })
+
 })
--- src/Router/ProfileRouter.ts
+++ src/Router/ProfileRouter.ts
@@ -1,6 +1,6 @@
 import { RouterClass } from "./Ultils/RouterClass";
 import { Request,Response } from "express";
-
+import Profile from "../Controller/Profile/Profile";
 export default class ProfileRouter extends RouterClass{
    public constructor(){
       super();
@@ -10,6 +10,29 @@ export default class ProfileRouter extends RouterClass{
    protected initializeRoutes(){
       this.router.get('/',(req:Request,res:Response)=>{
          res.send({message:"ProfileRouter"});
+      })
+
+      this.router.get("/getProfile",
+         this.validateToken,
+         (req:Request,res:Response)=>{
+            this.getProfile(req,res);
+         })
+   }
+
+   private async getProfile(req:Request,res:Response){
+      const user_id = req.body.authorizedUser.userId;
+      const userProfile:Profile = await Profile.GetByUserId({user_id:user_id});
+      res.send({
+            user_id:user_id,
+            user_name: userProfile.get_userName(),
+            first_name:userProfile.get_firstName(),
+            last_name:userProfile.get_lastName(),
+            contact:userProfile.get_contact(),
+            address:userProfile.get_address(),
+            membership_date:userProfile.get_memberDate(),
+            status:userProfile.get_status(),
+            total_fines:userProfile.get_totalFines(),
+            updated_at:userProfile.get_updatedAt(),
       })
    }
 }
