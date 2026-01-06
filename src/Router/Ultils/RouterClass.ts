a6b85e8843a888983cb4c4955b0b92bd05df16cc
pocoyo <joshuaelson46@gmail.com>
2026-01-06 21:04:04 +0800

    feat: Implement librarian role token validation
    - Add validateLibrarianToken middleware to test whether a request have a valid role
    - Refactor validateToken middleware

--- src/Router/Ultils/RouterClass.ts
+++ src/Router/Ultils/RouterClass.ts
@@ -1,5 +1,7 @@
 import { Router,Response,Request,NextFunction } from "express";
 import Env from "../../Config/config";
+import { UserJwtPayloadInterface } from "../../Config/config.interface";
+import { UserRole } from "../../Controller/User/User";
 export abstract class RouterClass {
     protected router: Router;
 
@@ -9,24 +11,36 @@ export abstract class RouterClass {
 
     public getRouter(): Router {
         return this.router;
-    }
+   }
 
-    protected validateToken(req: Request, res: Response, next: NextFunction) {
-        try {
-            const token = req.headers.authorization;
-            if (!token) throw new Error("Unauthorized Request");
-            if (!req.body) {
-                req.body = {
-                    authorizedUser: Env.getValidateToken(token),
-                };
-            } else {
-                req.body.authorizedUser = Env.getValidateToken(token);
-            }
-            next();
-        } catch (err: any) {
-            res.status(400).send({ error: err.message });
-        }
-    }
+  protected validateToken(req: Request, res: Response, next: NextFunction) {
+      try {
+         const token = req.headers.authorization;
+         if (!token) throw new Error("Unauthorized Request");
+         if (!req.body) req.body = {authorizedUser: Env.getValidateToken(token)};
+         req.body.authorizedUser = Env.getValidateToken(token);
+         next();
+      } catch (err: any) {
+         res.status(400).send({ error: err.message });
+      }
+   }
 
-    protected abstract initializeRoutes(): void;
+   protected validateLibrarianToken(req: Request, res: Response, next: NextFunction) {
+      function isLibrarian(payloadToken:UserJwtPayloadInterface):boolean{
+         if(payloadToken.userRole === UserRole.LIBRARIAN) return true;
+         return false;
+      }
+      try {
+         const token = req.headers.authorization;
+         if (!token) throw new Error("Unauthorized Request");
+         const parsedToken = Env.getValidateToken(token);
+         if (!isLibrarian(parsedToken)) throw new Error("Unauthorized Request")
+         if (!req.body) req.body = {authorizedAdmin: parsedToken};
+         req.body.authorizedAdmin = parsedToken;
+         next();
+      } catch (err: any) {
+         res.status(400).send({ error: err.message });
+      }
+   }
+   protected abstract initializeRoutes(): void;
 }
