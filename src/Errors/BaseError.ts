e9fecda2b63ffd9f3fb413564dae871c4aa30132
pocoyo <joshuaelson46@gmail.com>
2026-01-08 16:03:11 +0800

    feat: Implement error handling class (BaseError)
    - Add BaseError abstract class for consistent error handling
    - Support both client and developer error response

---
+++ src/Errors/BaseError.ts
@@ -0,0 +1,57 @@
+import { BaseErrorConstructorParams, ClientError, DevError } from "./types";
+import { HttpsStatusCode } from "./types";
+
+export abstract class BaseError extends Error{
+   public readonly message:string;
+   public readonly httpsStatusCode:HttpsStatusCode;
+   public readonly code:string;
+   public readonly isOperational:boolean;
+   public readonly context?: Record<string,any>;
+   public readonly timestamp: Date; 
+
+
+   constructor(param:BaseErrorConstructorParams){
+      super(param.message);
+
+      // Fix instance inheritence 
+      Object.setPrototypeOf(this,new.target.prototype);
+      
+      // Skip stack frame from BaseError
+      if(Error.captureStackTrace){
+         Error.captureStackTrace(this,this.constructor);
+      }
+
+      this.httpsStatusCode = param.httpStatusCode;
+      this.message = param.message;
+      this.code = param.code;
+      this.isOperational = param.isOperational;
+      this.context = param.context;
+      this.timestamp = new Date();
+      this.name = this.constructor.name;
+   }
+   
+   // For client error logs
+   public toClientResponse(): ClientError{
+      return{
+         name: this.name,
+         message:this.message,
+         code:this.code,
+         statusCode:this.httpsStatusCode,
+         timestamp:this.timestamp.toISOString()
+      }
+   }
+   
+   // For developer error logs
+   public toDevResponse(): DevError{
+      return{
+         name: this.name,
+         message:this.message,
+         code:this.code,
+         statusCode:this.httpsStatusCode,
+         stack:this.stack,
+         timestamp:this.timestamp.toISOString(),
+         context:this.context,
+         isOperational: this.isOperational,
+      } 
+   }
+}
