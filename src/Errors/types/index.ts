4a6e2d3a7742fb634124dce25d89a477b60da953
pocoyo <joshuaelson46@gmail.com>
2026-01-08 14:40:55 +0800

    chore: add barrel file for error types
    create src/Errors/types/index.ts to export all Error types ultilities

---
+++ src/Errors/types/index.ts
@@ -0,0 +1,8 @@
+export type { BaseErrorConstructorParams,ClientError,DevError } from "./BaseError.types";
+
+export type { 
+   InformationalStatusCode, SuccessStatusCode, RedirectionStatusCode, ClientErrorStatusCode,
+   ServerErrorStatusCode , HttpsStatusCode, HttpsErrorStatusCode, HttpsSuccessStatusCode
+} from "./httpsStatusCode.types";
+
+
