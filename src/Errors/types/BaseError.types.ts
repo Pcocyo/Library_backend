c51c984f1f9aeaa84c1e7f94742780cd38ce7103
pocoyo <joshuaelson46@gmail.com>
2026-01-08 14:37:47 +0800

    feat: add BaseError class type ultilities

---
+++ src/Errors/types/BaseError.types.ts
@@ -0,0 +1,34 @@
+import { HttpsStatusCode } from "./httpsStatusCode.types";
+
+//    message: string,              // User-friendly message
+//    httpStatusCode: number,       // 400, 404, 500, etc.
+//    code: string,                 // "VALIDATION_ERROR", "AUTH_FAILED"
+//    isOperational: boolean = true, // Default to operational errors
+//    context?: Record<string, any> 
+
+export interface BaseErrorConstructorParams {
+   message: string;
+   httpStatusCode: HttpsStatusCode;
+   code: string;
+   isOperational: boolean;
+   context?: Record<string,any>;
+}
+
+export interface ClientError{
+   name: string;
+   message: string;
+   code: string;
+   statusCode: HttpsStatusCode;
+   timestamp: string;
+}
+
+export interface DevError{
+   name: string;
+   message: string;
+   code: string;
+   statusCode: HttpsStatusCode;
+   stack?: string;
+   timestamp: string;
+   context?: Record<string,any>;
+   isOperational: boolean;
+}
