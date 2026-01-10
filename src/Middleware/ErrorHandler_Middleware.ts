759b6781a25c8c5ecb2870050e9316b980e67d49
pocoyo <joshuaelson46@gmail.com>
2026-01-10 11:28:41 +0800

    feat: Add ValidatePasswordParameter middleware to validatepassword parameter
    - Refactor .createInvalidInputError() function call to call the function with the correct parameter

--- src/Middleware/ErrorHandler_Middleware.ts
+++ src/Middleware/ErrorHandler_Middleware.ts
@@ -1,5 +1,5 @@
 import { Request,Response,NextFunction, RequestHandler } from "express";
-import { ValidationError, ValidationErrorFactory } from "../Errors";
+import { ValidationError, ValidationErrorCode, ValidationErrorFactory } from "../Errors";
 import { ClientErrorFactory } from "../Errors";
 import { ClientError } from "../Errors";
 import { ClientErrorResponse } from "../Errors/types";
@@ -15,14 +15,47 @@ export class ErrorHandler_Middleware{
          };
          const emailRegx: RegExp =
             /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-         if (!emailRegx.test(req.body.email)) {
+         if(!emailRegx.test(req.body.email)){
             throw ValidationErrorFactory.createInvalidInputError({
                field:"email",
                value:req.body.email,
+               context:{user_request_info:req.body},
+               code:ValidationErrorCode.Invalid_Email_Input
+            })
+         }
+         next();
+      }catch(error:any){
+         if(error instanceof ClientError){
+            const clientResponse:ClientErrorResponse = error.toClientResponse()
+            res.status(clientResponse.statusCode).json({...clientResponse});
+         };
+
+         if(error instanceof ValidationError){
+            const clientResponse:ClientErrorResponse = error.toClientResponse()
+            res.status(clientResponse.statusCode).json({...clientResponse});
+         };
+         res.json({...error.toClientResponse()});
+      }
+   }
+
+   public static ValidatePasswordParameter: RequestHandler = (req:Request,res:Response, next:NextFunction)=>{
+      try{
+         if(req.body.password == undefined){
+            throw ClientErrorFactory.createMissingFieldError({
+               field:"password",
                context:{user_request_info:req.body}
             });
          }
+         const passwordRegex:RegExp = 
+         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,32}$/;
+         if(!passwordRegex.test(req.body.password)){
+            throw ValidationErrorFactory.createInvalidInputError({
+               field:"password",
+               value:req.body.password,
+               context:{user_request_info:req.body},
+               code:ValidationErrorCode.Invalid_Password_Input
+            })
+         };
          next();
       }catch(error:any){
          if(error instanceof ClientError){
