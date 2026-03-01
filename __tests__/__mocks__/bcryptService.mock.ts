000c6479d07878ec81ae1a7b4ed66d41d74132a3
pocoyo <joshuaelson46@gmail.com>
2026-03-02 00:42:35 +0800

    test: replace bcrypt and jwt service mocks with fakes for behavior testing

--- __tests__/__mocks__/bcryptService.mock.ts
+++ __tests__/__mocks__/bcryptService.mock.ts
@@ -2,6 +2,7 @@ import { IBcryptService } from "../../src/core/security/interfaces";
 import BcryptService from "../../src/core/security/bcrypt.service";
 import { jest } from "@jest/globals";
 export const bcryptServiceMock_hashFnReturnValue = "hashPassword"
+
 export function createBcryptServiceMock(): IBcryptService {
     let bcryptService = new BcryptService({
         JWT_SECRET: "test-jwt-secret",
@@ -10,4 +11,13 @@ export function createBcryptServiceMock(): IBcryptService {
     jest.spyOn(bcryptService,"hashPassword").mockResolvedValue(bcryptServiceMock_hashFnReturnValue);
     jest.spyOn(bcryptService,"comparePassword");
     return bcryptService;
+}
+
+export function createBcryptServiceFakes():IBcryptService{
+   let bcryptService = new BcryptService({
+        JWT_SECRET: "test-jwt-secret",
+        BCRYPT_SALT: 1,
+   })
+
+   return bcryptService;
 }
--- __tests__/__mocks__/jwtService.mock.ts
+++ __tests__/__mocks__/jwtService.mock.ts
@@ -1,10 +1,22 @@
 import { IJwtService } from "../../src/core/security/interfaces";
 import JwtService from "../../src/core/security/jwt.service";
 
+export const jwtServiceMock_generateTokenReturn = "jwtTokenMock";
 export function createJwtServiceMock(): IJwtService {
     let jwtService: JwtService = new JwtService({
         JWT_SECRET: "test-secrets",
         BCRYPT_SALT:1,
     });
+
+    jwtService.generateJwtToken = jest.fn().mockReturnValue(jwtServiceMock_generateTokenReturn);
     return jwtService;
+}
+
+export function createJwtServiceFakes():IJwtService{
+    let jwtService: JwtService = new JwtService({
+        JWT_SECRET: "test-secrets",
+        BCRYPT_SALT:1,
+    });
+
+   return jwtService;
 }
